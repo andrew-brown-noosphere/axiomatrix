3fb00a7a230d669f5c590a21bfa2b5b8064d4650
andrew-brown-noosphere <andrew@noosphere.tech>
2026-03-08 05:27:09 -0400

    Fix: Remove SiteToggle import from page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import SiteToggle from "@/components/SiteToggle";
 import NewSite from "@/components/NewSite";
 
 export default function Home() {
-  return (
-    <>
-      <SiteToggle />
-      <NewSite />
-    </>
-  );
+  return <NewSite />;
 }
